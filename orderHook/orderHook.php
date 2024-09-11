3b28e680235c7d3f7c38e68d154fd3dda59b460a
Jean Pierre Soto Chirinos <jpsotochirinos@gmail.com>
2024-09-11 15:56:11 -0500

    add hook

--- orderHook/orderHook.php
+++ orderHook/orderHook.php
@@ -3,24 +3,22 @@ if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class MyModule extends Module
+class OrderHook extends Module
 {
     public function __construct()
     {
-        $this->name = 'orderHook'; // Nombre técnico del módulo
-        $this->tab = 'front_office_features'; // Categoría donde se muestra en el back office
-        $this->version = '1.0.0'; // Versión del módulo
-        $this->author = 'Jean'; // Autor del módulo
-        $this->need_instance = 0; // Si necesita cargar la instancia de clase en la página de módulos
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_); // Compatibilidad de versiones de PrestaShop
-        $this->bootstrap = true; // Usa Bootstrap en el back office
+        $this->name = 'orderhook';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0'; 
+        $this->author = 'Jean'; 
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_); 
+        $this->bootstrap = true;
 
         parent::__construct();
 
-        $this->displayName = $this->l('orderHook'); // Nombre mostrado en el back office
-        $this->description = $this->l('Descripción del módulo.'); // Descripción del módulo
-
-        $this->confirmUninstall = $this->l('¿Estás seguro de que quieres desinstalar?'); // Mensaje de confirmación para desinstalar
+        $this->displayName = $this->l('orderHook');
+        $this->description = $this->l('Descripci贸n del m贸dulo.'); 
     }
 
     public function install()
@@ -42,6 +40,43 @@ class MyModule extends Module
 
     public function hookActionPaymentConfirmation($params)
     {
+        PrestaShopLogger::addLog('Entro a la funcion');
+        PrestaShopLogger::addLog('Order : ', $params);
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://interpreter-dev.mytikray.com/orders/hook',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS =>'{
+            "Domain": "Marketplace",
+            "OrderId": "1440540507649-01",
+            "State": "payment-approved",
+            "LastState": "approve-payment",
+            "LastChange": "2024-06-19T20:57:20.8107666Z",
+            "CurrentChange": "2024-06-19T20:57:20.838933Z",
+            "Origin": {
+                "Account": "prodequa",
+                "Key": "vtexappkey-prodequa-ASJUUK"
+            }
+        }',
+          CURLOPT_HTTPHEADER => array(
+            'account: prodequa',
+            'Username: 6tncj079tdrbqfgadl3s8755f4',
+            'Password: 1eo1bolukrubtn2ocs6ivolhhpu3s0rum683689v2ole9cmnquba',
+            'Content-Type: application/json'
+          ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        echo $response;
+        
+         PrestaShopLogger::addLog('curl: ', $response);
         if (function_exists('mail')) {
             $content = print_r($params, true);
             mail("jean@prodequa.com", "Payment Confirmation", $content);
